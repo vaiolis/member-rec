02e83476ef6239411e0905150e5cc411243f5549
AdithyaG <incredibleatha2@gmail.com>
2022-08-21 09:13:27 -0700

    Update Question.jsx

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { doc ,getDoc, getFirestore } from 'firebase/firestore';
+import { doc, getDoc, getFirestore } from 'firebase/firestore';
 import { getStorage, ref, getDownloadURL } from 'firebase/storage';
 import React from 'react';
 import './Question.css';
@@ -7,14 +7,14 @@ import './NameForm';
 import NameForm from './NameForm';
 
 function Question(props) {
-    console.log("started");
+  console.log('started');
   const firebaseConfig = {
-    apiKey: "AIzaSyAktEso_fJ-wePG4AGcRgo2IBezCKq5cJY",
-    authDomain: "face-rec-js.firebaseapp.com",
-    projectId: "face-rec-js",
-    storageBucket: "face-rec-js.appspot.com",
-    messagingSenderId: "75624240447",
-    appId: "1:75624240447:web:31018c9429dd94db666f8a"
+    apiKey: 'AIzaSyAktEso_fJ-wePG4AGcRgo2IBezCKq5cJY',
+    authDomain: 'face-rec-js.firebaseapp.com',
+    projectId: 'face-rec-js',
+    storageBucket: 'face-rec-js.appspot.com',
+    messagingSenderId: '75624240447',
+    appId: '1:75624240447:web:31018c9429dd94db666f8a',
   };
   const app = initializeApp(firebaseConfig);
   const storage = getStorage(app);
@@ -26,21 +26,21 @@ function Question(props) {
   var randList;
   var officers;
   function initConnection() {
-    const docRef = doc(db,'main','People');
+    const docRef = doc(db, 'main', 'People');
     getDoc(docRef).then((docData) => {
-        try {
-          console.log(docData);
-          data = docData.data();
-          console.log(data);
-        } catch (error) {
-          console.log(error);
-        }
-        officers = data.Officers;
-        randList = genRandList(officers.length)
-        currImg = officers[randList[index]].ImageUrl;
-        console.log(currImg);
-        correctName = officers[randList[index]].name;
-        document.getElementById("PersonImg").setAttribute("src",currImg);
+      try {
+        console.log(docData);
+        data = docData.data();
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+      officers = data.Officers;
+      randList = genRandList(officers.length);
+      currImg = officers[randList[index]].ImageUrl;
+      console.log(currImg);
+      correctName = officers[randList[index]].name;
+      document.getElementById('PersonImg').setAttribute('src', currImg);
     });
   }
   function nextQuestion() {
@@ -52,17 +52,17 @@ function Question(props) {
   function genRandList(range) {
     randList = new Array();
     var numList = new Array();
-    for(let i = 0; i < range; i++) {
-       numList.push(i);
+    for (let i = 0; i < range; i++) {
+      numList.push(i);
     }
-    for(let i = 0; i < range; i++) {
+    for (let i = 0; i < range; i++) {
       var randInd = Math.floor(Math.random() * numList.length);
       randList.push(randInd);
-      numList.splice(i,1);
+      numList.splice(i, 1);
     }
     return randList;
   }
-  
+
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => initConnection(), []);
 
