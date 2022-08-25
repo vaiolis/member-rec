9d474022bc0fafe8395b7d2c36e54aad36b04c37
Arnav Chakravarthy <achakravarthy298@student.fuhsd.org>
2022-08-25 16:52:19 -0700

    results route + scorecard + mobile responsive updates

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import './Results.css';
+// import NameForm from '../components/NameForm';
 
 export default function Results() {
   return (
     <div>
-      <h1>Results</h1>
+      <h1 className="header">Your Scorecard</h1>
+      <div className="results">
+        <div className="results-card">
+          <div className="results-container">
+            <div className="results-row">
+              <div className="name">
+                <strong>Score</strong>
+              </div>
+              <div className="score">
+                <strong>###</strong>
+              </div>
+            </div>
+            <div className="results-row">
+              <div className="name">
+                <strong>Enter your Name</strong>
+              </div>
+              <div className="score">
+                <input name="name"></input>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button className="score-submit">Submit!</button>
     </div>
   );
 }
