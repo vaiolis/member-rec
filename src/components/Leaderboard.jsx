b3cf5708bb05beee09f83905492f975251c0f14a
Arnav Chakravarthy <achakravarthy298@student.fuhsd.org>
2023-01-12 16:10:53 -0800

    Update Leaderboard.jsx
    - level incorporated
    - style changes

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -12,15 +12,17 @@ export default function Leaderboard() {
     const list = docData.data().Scores.sort((a, b) => (a.Score < b.Score ? 1 : -1));
     const topTen = list.slice(0, 10);
       setItems(
-        topTen.map((entry, index) => (
-          <>
-            <div className="grid grid-cols-2" key={index}>
-              <div className="text-lg my-5">{entry.Name}</div>
-              <div className="text-lg my-5">{entry.Score}</div>
-            </div>
-            <hr className="h-px bg-gray-200 border-0 border-dotted dark:bg-gray-500"></hr>
-          </>
-        ))
+       topTen.map((entry, index) => (
+              <>
+                <div className="flex flex-row justify-between" key={index}>
+                  <div className="text-lg my-5 text-purple-300">{entry.Name}</div>
+                  <div className="text-lg my-5 text-purple-300">{entry.Score}</div>
+                  <div className="text-lg my-5 text-purple-300">{entry.Level}</div>
+                </div>
+                <hr className="h-px bg-gray-200 border-0 border-dotted dark:bg-gray-500"></hr>
+              </>
+            ))
+        
       );
     });
   }, [docRef]);
@@ -28,7 +30,12 @@ export default function Leaderboard() {
   return (
     <div className="px-10 py-14">
       <h1 className='font-bold text-2xl text-blue-700'>Leaderboard</h1>
-        {listItems}
+      <div className="flex flex-row justify-between">
+        <div className="font-bold text-lg text-purple-500 my-5">Name</div>
+        <div className="font-bold text-lg text-blue-500 my-5">Score</div>
+        <div className="font-bold text-lg text-orange-500 my-5">Level</div>
+      </div>
+      {listItems}
     </div>
   );
 }
